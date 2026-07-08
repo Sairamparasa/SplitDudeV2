2538aa923704a520ed67c8cd92b4477f49932111
Sairamparasa <parasasairam2@gmail.com>
2026-07-08 22:23:41 +0530

    Update schema definition for group_members table to include role column

--- supabase-schema.sql
+++ supabase-schema.sql
@@ -36,6 +36,7 @@ create table public.groups (
 create table public.group_members (
   group_id uuid references public.groups(id) on delete cascade,
   user_id uuid references public.profiles(id) on delete cascade,
+  role text default 'member',
   joined_at timestamp with time zone default timezone('utc'::text, now()) not null,
   primary key (group_id, user_id)
 );
